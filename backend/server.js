c1e4bb5f37313b8cfa59b8d0aeecf85787e6867a
jimmypuli <armg296@gmail.com>
2025-02-18 18:04:55 -0500

    updates

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,6 @@ dotenv.config();
 const app = express(); 
 const PORT = process.env.PORT || 5000; 
 
-console.log(process.env.MONGO_URI); 
-
 app.use("/api/auth", authRoutes); 
 
 app.listen(PORT, () => {
